6b2922bdbe50ec3c3cbc867dfd07bb8e821830ea
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-06-19 16:27:00 +0200

    Added 2 buttons to homepage which navigate to new explore page and sign up

---
+++ src/pages/Explore/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+
+export default function Explore() {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Explore</h1>
+      </Jumbotron>
+    </div>
+  );
+}
