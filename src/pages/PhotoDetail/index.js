603dd7173051c19c13d90203aae56d0c5c4ea2bb
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-07-14 11:47:01 +0200

    styled photodetail and added backlink to gallery

--- src/pages/PhotoDetail/index.js
+++ src/pages/PhotoDetail/index.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
-import { Jumbotron, Container, Row, Col } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Jumbotron, Container, Row, Col, Button } from "react-bootstrap";
+import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getSinglePhoto } from "../../store/photo/actions";
 import { selectSinglePhoto } from "../../store/photo/selectors";
 import CartButtons from "../../components/CartButtons";
+import "./styling.css";
 
 export default function PhotoDetail() {
-  const { id } = useParams();
+  const { id, name } = useParams();
   const dispatch = useDispatch();
   const photo = useSelector(selectSinglePhoto);
 
@@ -16,23 +17,29 @@ export default function PhotoDetail() {
   }, [dispatch, id]);
 
   return (
-    <div style={{ paddingBottom: "20px" }}>
+    <div className="page">
       <Jumbotron>
         <h1>Details</h1>
       </Jumbotron>
       <Container>
-        <Row>
-          <Col>
-            <img src={photo.src} alt={photo.info} />
-          </Col>
-          <Col>
-            <h3>Info</h3>
-            <p>{photo.info}</p>
-            <h3>Artist Description:</h3>
-            <p>{photo.description}</p>
-            <CartButtons id={photo.id} src={photo.src} />
-          </Col>
-        </Row>
+        <div className="detail">
+          <Row>
+            <Col>
+              <img src={photo.src} alt={photo.info} />
+            </Col>
+            <Col>
+              <h3>Info</h3>
+              <p>{photo.info}</p>
+              <h3>Artist Description:</h3>
+              <p>{photo.description}</p>
+              <CartButtons id={photo.id} src={photo.src} />
+              <p></p>
+              <Link to={`/gallery/${name}`}>
+                <Button variant="dark">Back to Gallery</Button>
+              </Link>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </div>
   );
