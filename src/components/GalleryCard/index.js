d2ad5461d016f849565854efc37aae2ce3cee5bb
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-07-14 09:38:23 +0200

    added styling in seperate sheet

--- src/components/GalleryCard/index.js
+++ src/components/GalleryCard/index.js
@@ -1,24 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./styling.css";
 
 export default function GalleryCard(props) {
-  const imgStyle = {
-    width: "32%",
-    margin: "5px",
-  };
   return (
-    <div style={{ margin: "5px", padding: "10px" }}>
+    <div className="galleryCard">
       <h3>{props.name}</h3>
       <p>Photos: {props.photos.length}</p>
       {props.photos.slice(0, 3).map((photo) => {
-        return (
-          <img
-            key={photo.id}
-            src={photo.src}
-            style={imgStyle}
-            alt={photo.info}
-          />
-        );
+        return <img key={photo.id} src={photo.src} alt={photo.info} />;
       })}
 
       <Link to={`/gallery/${props.name}`}>
