74d0e3d4b972e932ddb9adecf3d7dc3d9b7d59a5
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-07-15 16:52:15 +0200

    refactore state management for photo detail page

--- src/store/photo/actions.js
+++ src/store/photo/actions.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import { apiUrl } from "../../config/constants";
 import { selectToken } from "../user/selectors";
+import { selectSingleGallery } from "../galleries/selectors";
 
 export const storeSinglePhoto = (photo) => ({
   type: "STORE_PHOTO",
@@ -8,12 +9,19 @@ export const storeSinglePhoto = (photo) => ({
 });
 
 export const getSinglePhoto = (id) => async (dispatch, getState) => {
-  try {
-    const response = await Axios.get(`${apiUrl}/photos/${id}`);
-    //console.log("Photo gotten from database:", response.data);
-    dispatch(storeSinglePhoto(response.data));
-  } catch (e) {
-    console.log(e);
+  const gallery = selectSingleGallery(getState());
+  if (Object.keys(gallery).length) {
+    const photoFound = gallery.photos.find(
+      (photo) => photo.id === parseInt(id)
+    );
+    dispatch(storeSinglePhoto(photoFound));
+  } else {
+    try {
+      const response = await Axios.get(`${apiUrl}/photos/${id}`);
+      dispatch(storeSinglePhoto(response.data));
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
 
