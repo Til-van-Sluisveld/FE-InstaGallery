1f549a9e71f3762aa74500ac3f8cfc8c8170f984
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-08-20 16:44:26 +0200

    Added delete button in my-gallery when signed in

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./styling.css";
+import { selectUser } from "../../store/user/selectors";
+import { useSelector } from "react-redux";
+import { Button } from "react-bootstrap";
 
 export default function PhotoCard(props) {
+  const user = useSelector(selectUser);
+  console.log("user.name:", user.name);
+  console.log("props.handle:", props.handle);
+
   return (
     <div className="photo-card">
       <Link to={`/gallery/${props.handle}/${props.id}`}>
         <img src={props.src} alt={props.info} />
       </Link>
+      {user.name === props.handle ? (
+        <Button style={{ marginTop: 10 }} variant="dark">
+          Delete
+        </Button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
