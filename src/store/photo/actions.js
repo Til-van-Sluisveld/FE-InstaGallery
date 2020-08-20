d3e8d81b4fb189699be85030a5bcea087386feb4
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-08-20 18:05:27 +0200

    Updates state when photo gets deleted

--- src/store/photo/actions.js
+++ src/store/photo/actions.js
@@ -1,7 +1,8 @@
 import Axios from "axios";
 import { apiUrl } from "../../config/constants";
-import { selectSingleGallery } from "../galleries/selectors";
+import { selectSingleGallery, selectGalleries } from "../galleries/selectors";
 import { selectToken } from "../user/selectors";
+import { storeSingleGallery } from "../galleries/actions";
 
 export const storeSinglePhoto = (photo) => ({
   type: "STORE_PHOTO",
@@ -27,11 +28,17 @@ export const getSinglePhoto = (id) => async (dispatch, getState) => {
 
 export const deletePhoto = (id) => async (dispatch, getState) => {
   const token = selectToken(getState());
+  const oldGallery = selectSingleGallery(getState());
+  const newGallery = {
+    ...oldGallery,
+    photos: oldGallery.photos.filter((photo) => photo.id !== id),
+  };
   try {
     const response = await Axios.delete(`${apiUrl}/photos/delete/${id}`, {
       headers: { Authorization: `Bearer ${token}` },
     });
     console.log("response:", response);
+    dispatch(storeSingleGallery(newGallery));
   } catch (e) {
     console.log(e);
   }
